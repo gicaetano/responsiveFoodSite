6e298e8dfc835e280ac79f45d801aec281303d02
Gisele Caetano Pereira <87086783+gicaetano@users.noreply.github.com>
2021-11-18 13:39:00 -0300

    :package: ResponsiveOn

--- js/app.js
+++ js/app.js
@@ -5,24 +5,24 @@ $(document).ready(function(){
         slidesToScroll:1,
         prevArrow:".prev-btn",
         nextArrow:".next-btn",
-    //   responsive:[
-    //      {
-    //         breakpoint:992,
-    //         settings:{
-    //          slidesToShow:2,
-    //         }
-    //      },
-    //      {
-    //       breakpoint:768,
-    //       settings:{
-    //        slidesToShow:1,
-    //       }
-    //    }
-    //   ]
+      responsive:[
+         {
+            breakpoint:992,
+            settings:{
+             slidesToShow:2,
+            }
+         },
+         {
+          breakpoint:768,
+          settings:{
+           slidesToShow:1,
+          }
+       }
+      ]
  
-    // });
+    });
  
-    // $('.nav-trigger').click(function(){
-    //    $('.site-content-wrapper').toggleClass('scaled');
+    $('.nav-trigger').click(function(){
+       $('.site-content-wrapper').toggleClass('scaled');
     })
  });
